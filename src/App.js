580736fdb7c20357e6d9972327329949f1e3878c
Rajeev Ranjan <itisrajeevsingh@gmail.com>
2022-11-21 18:27:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+import LazyLoad from "react-lazyload";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Spinner = () => (
+  <div className="post loading">
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke="#49d1e0"
+        strokeWidth="10"
+        r="35"
+        strokeDasharray="164.93361431346415 56.97787143782138"
+        transform="rotate(275.845 50 50)"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          calcMode="linear"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+          dur="1s"
+          begin="0s"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </svg>
+  </div>
+);
+
+const UserProfile = ({ id, name, email, website, company, phone }) => (
+  <div className="post">
+    <LazyLoad
+      once={true}
+      placeholder={<img src={`https://avatars.dicebear.com/api/male/${id}.svg`} alt="..." />}
+    >
+      <div className="post-img">
+        <img src={`https://avatars.dicebear.com/api/male/${id}.svg`} alt="..." />
+      </div>
+    </LazyLoad>
+    <div className="post-body">
+    <br></br>
+      <h3>Name: {name}</h3>
+      <br></br>
+      <p>Email : {email}</p>
+      <p>Website : {website}</p>
+       <p>Company : {company.name} </p>
+       <br></br>
+       <h4>Call At : {phone} </h4>
     </div>
-  );
-}
+  </div>
+);
+
+const App = () => (
+  <div className="App">
+    <h2>React Lazy Load from Fake Rest API - By Rajeev Ranjan</h2>
+    <div className="post-container">
+      {
+        data.map(user => (
+        <LazyLoad
+          key={user.id}
+          height={100}
+          offset={[-100, 100]}
+          placeholder={<Spinner />}
+        >
+          <UserProfile key={user.id} {...user} />
+        </LazyLoad>
+      ))}
+    </div>
+  </div>
+);
 
 export default App;
+    
